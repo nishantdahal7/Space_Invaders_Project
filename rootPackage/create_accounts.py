da52c743586d3a2c01c3126f7d234d27cf0bc5da
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2021-09-26 12:22:34 +0545

    some bugs are fixed.

--- rootPackage/create_accounts.py
+++ rootPackage/create_accounts.py
@@ -1,3 +1,6 @@
+"""
+This modules accepts user's full name, username and password for registration.
+"""
 import tkinter
 import tkinter.messagebox
 from tkinter import messagebox
@@ -12,6 +15,7 @@ def link():
     root.resizable(width=False, height=False)
     root.configure(bg="green")
     bg = PhotoImage(file="imag_bg.png")
+
     # Creating Canvas
     my_canvas = Canvas(root, width=800, height=600, highlightthickness=0)
     my_canvas.pack(fill="both", expand=True)
@@ -36,11 +40,11 @@ def link():
     #Setting Canvas Image
     #  my_canvas.create_image(0,0, image=backGround, anchor="nw")
     #Creating Label In Canvas
-    my_canvas.create_text(400, 60, text="CREATE AN ACCOUNT", font=("arial",30,'bold'), fill="blue")
-    my_canvas.create_text(300, 140, text="FULL NAME", font=("helvetica",13, 'bold'), fill="grey")
-    my_canvas.create_text(300, 235, text="USERNAME", font=("helvetica",13,'bold'), fill="grey")
-    my_canvas.create_text(300, 330, text="PASSWORD", font=("helvetica",13,'bold'), fill="grey")
-    my_canvas.create_text(340, 425, text="CONFIRM PASSWORD", font=("helvetica",13,'bold'), fill="grey")
+    my_canvas.create_text(400, 60, text="CREATE AN ACCOUNT", font=("copperplate",30,'bold'), fill="yellow")
+    my_canvas.create_text(400, 140, text="FULL NAME", font=("helvetica",20, 'bold'), fill="white")
+    my_canvas.create_text(400, 235, text="USERNAME", font=("helvetica",20,'bold'), fill="white")
+    my_canvas.create_text(400, 330, text="PASSWORD", font=("helvetica",20,'bold'), fill="white")
+    my_canvas.create_text(400, 425, text="CONFIRM PASSWORD", font=("helvetica",20,'bold'), fill="white")
     #Defining Entry Boxes
     name_entry = Entry(root, bd=0.5, bg="black", fg="white", font=("cambria", 22, 'italic'), insertbackground="white")
     username_entry = Entry(root, bd=0.5, bg="black", fg="white", font=("cambria", 22, 'italic'), insertbackground="white")
@@ -64,21 +68,21 @@ def link():
 
     def register():
         if name_entry.get() == "" and username_entry.get() == "" and password_entry.get() == "" and confirm_password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Password.")
         elif name_entry.get() == "" and username_entry.get() == "" and password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Password.")
         elif name_entry.get() == "" and username_entry.get() == "" and confirm_password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Confirm Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username,Confirm Password.")
         elif name_entry.get() == "" and username_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username Correctly")
-        elif username_entry.get() == "" and password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Username,Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Username.")
+        elif username_entry.get() == "" and password_entry.get() == "" and confirm_password_entry.get() == "":
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Username,Password and Confirm password.")
         elif name_entry.get() == "" and password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Name,Password.")
         elif password_entry.get() == "" and confirm_password_entry.get() == "":
-            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Your Password Correctly")
+            tkinter.messagebox.showinfo("INVALID DETAILS", "Please Enter Your Password.")
         elif password_entry.get() != confirm_password_entry.get():
-            tkinter.messagebox.showinfo("INVALID PASSWORD", "Please Enter Your Password Correctly!")
+            tkinter.messagebox.showinfo("INVALID PASSWORD", "Password and Confirm password do not match.")
         elif name_entry.get() == "":
             tkinter.messagebox.showinfo("NAME EMPTY", "Please Enter Your Name Correctly!")
         elif username_entry.get() == "":
@@ -93,23 +97,12 @@ def link():
             connet_me.commit()
             connet_me.close()
             messagebox.showinfo("Registration Successfully.", "Your account has been created successfully.")
-
+            root.destroy()
 
 
     #Creating Register Button
-    register_button = Button(root, text="REGISTER", bd=3, bg="black", fg="red", font=("arial", 15, 'bold'), width=15, height=2, command=register)
-    register_window = my_canvas.create_window(310, 500, anchor="nw", window=register_button)
-
-    '''
-    # Creating Label
-    my_label = Label(root, image=bg)
-    my_label.place(x=0, y=0,  relwidth=1, relheight=1)
-    # Creating Account
-    my_house = Label(root, text="Create An Account", Font=("arial", 40, "bold")
-    my_house.pack(pady=40)
-    '''
-    #Commiting Changes
-        #database.commit()
+    register_button = Button(root, text="REGISTER", bd=3, highlightbackground="green", fg="red", font=("arial", 15, 'bold'), width=15, height=2, command=register)
+    register_window = my_canvas.create_window(340, 500, anchor="nw", window=register_button)
 
     root.mainloop()
 
